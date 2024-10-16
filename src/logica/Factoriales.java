17d3ee9f4ae3cecabb9e59e0c28deaac953016d5
codsebas <arosalesr13@miumg.edu.gt>
2024-10-16 00:59:46 -0600

    Factoriales y añadir iconos

--- src/logica/Factoriales.java
+++ src/logica/Factoriales.java
@@ -1,37 +1,42 @@
 package logica;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 import modelos.ModeloFactorial;
 
-public class Factoriales implements ActionListener, WindowListener{
-    
+public class Factoriales implements ActionListener, WindowListener {
+
     ModeloFactorial modelo;
+    int tipoFactorial;
 
     public Factoriales(ModeloFactorial modelo) {
         this.modelo = modelo;
     }
-    
-    
-    public static long factorial(int numero){
-        if(numero <= 1){
+
+    // Función con el método general de los factoriales utilizando recursividad
+    public static long factorial(int numero) {
+        if (numero <= 1) {
             return 1;
         } else {
             return numero * factorial(numero - 1);
         }
     }
-    
+
+    //Formato libre 
     public static String procesarExpresion(String expresion) {
         // Buscar los factoriales usando Regex y reemplazarlos con su valor
         Pattern p = Pattern.compile("(\\d+)!");
         Matcher m = p.matcher(expresion);
 
         while (m.find()) {
-            // Capturamos el número antes del símbolo de factorial
+            // Capturar número antes del símbolo de factorial
             int numero = Integer.parseInt(m.group(1));
             // Calculamos el factorial
             long resultadoFactorial = factorial(numero);
@@ -53,20 +58,106 @@ public class Factoriales implements ActionListener, WindowListener{
         // Usar el motor JavaScript para evaluar la expresión matemática
         javax.script.ScriptEngineManager manager = new javax.script.ScriptEngineManager();
         javax.script.ScriptEngine engine = manager.getEngineByName("JavaScript");
-        
+
         Object resultado = engine.eval(expresion);
         return resultado.toString();
     }
 
+    //Método para verificar que es lo que se quiere resolver
+    public int Opcion() {
+        String[] opciones = {
+            "a) n! = n · (n-1)!",
+            "b) x! = n! → x = n",
+            "c) n! / n = (n-1)!",
+            "d) n! / (n-1)! = n",
+            "e) 0! = 1"
+        };
+
+        // Mostrar JOptionPane con las opciones
+        int seleccion = JOptionPane.showOptionDialog(
+                null,
+                "Seleccione una opción para calcular el factorial",
+                "Cálculo de Factorial",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                opciones,
+                opciones[0]
+        );
+
+        // Asignar un correlativo dependiendo de la selección del usuario
+        int correlativo = 0;
+
+        switch (seleccion) {
+            case 0:
+                correlativo = 1; // Opción a
+                break;
+            case 1:
+                correlativo = 2; // Opción b
+                break;
+            case 2:
+                correlativo = 3; // Opción c
+                break;
+            case 3:
+                correlativo = 4; // Opción d
+                break;
+            case 4:
+                correlativo = 5; // Opción e
+                break;
+            default:
+                correlativo = 6;
+                break;
+        }
+        return correlativo;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getActionCommand().equals(modelo.getVista().btnCalcular.getActionCommand())){
+        if (e.getActionCommand().equals(modelo.getVista().btnAyuda.getActionCommand())) {
+            modelo.getVista().wdwMensaje.setTitle("Ayuda");
+            modelo.getVista().wdwMensaje.setLocationRelativeTo(null);
+            modelo.getVista().wdwMensaje.setVisible(true);
+        }
+        if (e.getActionCommand().equals(modelo.getVista().btnCalcular.getActionCommand()) && tipoFactorial > 5) {
             modelo.getVista().txtResultado.setText(procesarExpresion(modelo.getVista().txtExpresion.getText()));
+        } else if (e.getActionCommand().equals(modelo.getVista().btnCalcular.getActionCommand()) && tipoFactorial == 1) {
+            modelo.getVista().txtResultado.setText(String.valueOf(factorial(Integer.parseInt(modelo.getVista().txtExpresion.getText()))));
+        } else if (e.getActionCommand().equals(modelo.getVista().btnCalcular.getActionCommand()) && tipoFactorial == 2) {
+            modelo.getVista().txtResultado.setText(modelo.getVista().txtExpresion.getText() + " = " + modelo.getVista().txtExpresion2.getText() +
+                    " factorial = " + procesarExpresion(modelo.getVista().txtExpresion.getText()));
+        } else if (e.getActionCommand().equals(modelo.getVista().btnCalcular.getActionCommand()) && tipoFactorial == 3) {
+        } else if (e.getActionCommand().equals(modelo.getVista().btnCalcular.getActionCommand()) && tipoFactorial == 4) {
+        } else if (e.getActionCommand().equals(modelo.getVista().btnCalcular.getActionCommand()) && tipoFactorial == 5) {
+            modelo.getVista().txtResultado.setText(String.valueOf(factorial(Integer.parseInt(modelo.getVista().txtExpresion.getText()))));
         }
     }
 
     @Override
     public void windowOpened(WindowEvent e) {
+        modelo.getVista().setLocationRelativeTo(null);
+        modelo.getVista().setVisible(false);
+        tipoFactorial = Opcion();
+        switch (tipoFactorial) {
+            case 1:
+                modelo.getVista().txtTitulo.setText("Factorial caso n!");
+                break;
+            case 2:
+                modelo.getVista().txtTitulo.setText("Factorial caso x! = n!");
+                break;
+            case 3:
+                modelo.getVista().txtTitulo.setText("Factorial caso n! / n = (n-1)!");
+                break;
+            case 4:
+                modelo.getVista().txtTitulo.setText("Factorial caso n! / (n-1)! = n");
+                break;
+            case 5:
+                modelo.getVista().txtTitulo.setText("Factorial caso 0! = 1");
+                break;
+            default:
+                modelo.getVista().txtTitulo.setText("Factorial con formato libre");
+                break;
+        }
+        modelo.getVista().setVisible(true);
     }
 
     @Override
--- src/vistas/VistaFactorial.java
+++ src/vistas/VistaFactorial.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package vistas;
+import javax.swing.ImageIcon;
 import modelos.ModeloFactorial;
 import logica.Factoriales;
 
@@ -20,6 +21,7 @@ public class VistaFactorial extends javax.swing.JFrame {
         ModeloFactorial modelo = new ModeloFactorial(this);
         Factoriales factoriales = new Factoriales(modelo);
         setFactoriales(factoriales);
+        ImageIcon help_icon = new ImageIcon("../iconos/help_icon.png");
     }
 
     /**
@@ -31,56 +33,163 @@ public class VistaFactorial extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        btnCalcular = new javax.swing.JButton();
+        wdwMensaje = new javax.swing.JDialog();
         jScrollPane1 = new javax.swing.JScrollPane();
-        txtResultado = new javax.swing.JTextArea();
+        jTextArea1 = new javax.swing.JTextArea();
+        jLabel2 = new javax.swing.JLabel();
+        panelFactorialSimple = new javax.swing.JPanel();
+        txtTitulo = new javax.swing.JLabel();
         txtExpresion = new javax.swing.JTextField();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        txtResultado = new javax.swing.JTextArea();
+        btnAyuda = new javax.swing.JButton();
+        btnCalcular = new javax.swing.JButton();
+        btnLimpiar = new javax.swing.JButton();
+        txtExpresion2 = new javax.swing.JTextField();
+        txtDivisor = new javax.swing.JLabel();
+        txtDividendo = new javax.swing.JLabel();
+
+        wdwMensaje.setTitle("Ayuda");
+        wdwMensaje.setLocation(new java.awt.Point(0, 0));
+        wdwMensaje.setSize(new java.awt.Dimension(629, 330));
+
+        jTextArea1.setEditable(false);
+        jTextArea1.setColumns(20);
+        jTextArea1.setLineWrap(true);
+        jTextArea1.setRows(5);
+        jTextArea1.setText("Ingresa un número entero positivo:\nAsegúrate de que el número que ingresas sea un entero no negativo. El factorial solo se puede calcular para números enteros. Por ejemplo:\n\nCorrecto: 0, 1, 5, 10\nIncorrecto: -1, 3.5, texto, 5.5\nHaz clic en el botón \"Calcular\":\nDespués de ingresar el número, presiona el botón \"Calcular\" para obtener el resultado.\n\nVisualiza el resultado:\nEl resultado del cálculo se mostrará en la pantalla. Si el número es correcto, verás el factorial correspondiente. Si el número es inválido, aparecerá un mensaje de error indicando que solo se permiten números enteros no negativos.");
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jLabel2.setFont(new java.awt.Font("Yu Gothic UI Semibold", 1, 24)); // NOI18N
+        jLabel2.setText("Ayuda");
+
+        javax.swing.GroupLayout wdwMensajeLayout = new javax.swing.GroupLayout(wdwMensaje.getContentPane());
+        wdwMensaje.getContentPane().setLayout(wdwMensajeLayout);
+        wdwMensajeLayout.setHorizontalGroup(
+            wdwMensajeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(wdwMensajeLayout.createSequentialGroup()
+                .addGap(279, 279, 279)
+                .addComponent(jLabel2)
+                .addContainerGap(281, Short.MAX_VALUE))
+            .addGroup(wdwMensajeLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1)
+                .addContainerGap())
+        );
+        wdwMensajeLayout.setVerticalGroup(
+            wdwMensajeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, wdwMensajeLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 268, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18))
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setFont(new java.awt.Font("Yu Gothic UI Semibold", 1, 36)); // NOI18N
-        jLabel1.setText("Factorial");
+        panelFactorialSimple.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
-        btnCalcular.setText("calcular");
+        txtTitulo.setFont(new java.awt.Font("Yu Gothic UI Semibold", 1, 36)); // NOI18N
+        txtTitulo.setText("Factoriales simples");
+
+        txtExpresion.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                txtExpresionFocusGained(evt);
+            }
+        });
 
         txtResultado.setColumns(20);
         txtResultado.setRows(5);
-        jScrollPane1.setViewportView(txtResultado);
+        jScrollPane2.setViewportView(txtResultado);
+
+        btnAyuda.setIcon(new javax.swing.ImageIcon(getClass().getResource("/iconos/help_question_32.png"))); // NOI18N
+
+        btnCalcular.setText("calcular");
+
+        btnLimpiar.setText("Limpiar");
+
+        txtDivisor.setText("Divisor:");
+
+        txtDividendo.setText("Dividendo");
+
+        javax.swing.GroupLayout panelFactorialSimpleLayout = new javax.swing.GroupLayout(panelFactorialSimple);
+        panelFactorialSimple.setLayout(panelFactorialSimpleLayout);
+        panelFactorialSimpleLayout.setHorizontalGroup(
+            panelFactorialSimpleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelFactorialSimpleLayout.createSequentialGroup()
+                .addContainerGap(90, Short.MAX_VALUE)
+                .addGroup(panelFactorialSimpleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelFactorialSimpleLayout.createSequentialGroup()
+                        .addComponent(btnAyuda)
+                        .addContainerGap())
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelFactorialSimpleLayout.createSequentialGroup()
+                        .addComponent(txtTitulo)
+                        .addGap(175, 175, 175))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelFactorialSimpleLayout.createSequentialGroup()
+                        .addGroup(panelFactorialSimpleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(btnCalcular)
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 341, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(panelFactorialSimpleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelFactorialSimpleLayout.createSequentialGroup()
+                                    .addComponent(txtDividendo)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(txtExpresion, javax.swing.GroupLayout.PREFERRED_SIZE, 341, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelFactorialSimpleLayout.createSequentialGroup()
+                                    .addComponent(txtDivisor)
+                                    .addGap(47, 47, 47)
+                                    .addComponent(txtExpresion2, javax.swing.GroupLayout.PREFERRED_SIZE, 341, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                        .addGap(18, 18, 18)
+                        .addComponent(btnLimpiar)
+                        .addGap(72, 72, 72))))
+        );
+        panelFactorialSimpleLayout.setVerticalGroup(
+            panelFactorialSimpleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelFactorialSimpleLayout.createSequentialGroup()
+                .addGap(7, 7, 7)
+                .addComponent(btnAyuda)
+                .addGap(12, 12, 12)
+                .addComponent(txtTitulo)
+                .addGap(18, 18, 18)
+                .addGroup(panelFactorialSimpleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtExpresion, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtDividendo))
+                .addGap(18, 18, 18)
+                .addGroup(panelFactorialSimpleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtExpresion2, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtDivisor))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(panelFactorialSimpleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnCalcular)
+                    .addComponent(btnLimpiar))
+                .addGap(86, 86, 86))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(82, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(166, 166, 166))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(txtExpresion, javax.swing.GroupLayout.PREFERRED_SIZE, 368, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(btnCalcular)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 368, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(49, 49, 49))))
+                .addContainerGap()
+                .addComponent(panelFactorialSimple, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(14, 14, 14)
-                .addComponent(jLabel1)
-                .addGap(33, 33, 33)
-                .addComponent(txtExpresion, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(btnCalcular)
-                .addGap(15, 15, 15))
+                .addContainerGap()
+                .addComponent(panelFactorialSimple, javax.swing.GroupLayout.PREFERRED_SIZE, 489, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void txtExpresionFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtExpresionFocusGained
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtExpresionFocusGained
 
     /**
      * @param args the command line arguments
@@ -119,14 +228,25 @@ public class VistaFactorial extends javax.swing.JFrame {
     
     public void setFactoriales(Factoriales factorial){
         btnCalcular.addActionListener(factorial);
+        btnAyuda.addActionListener(factorial);
         this.addWindowListener(factorial);
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    public javax.swing.JButton btnAyuda;
     public javax.swing.JButton btnCalcular;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton btnLimpiar;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextArea jTextArea1;
+    public javax.swing.JPanel panelFactorialSimple;
+    public javax.swing.JLabel txtDividendo;
+    public javax.swing.JLabel txtDivisor;
     public javax.swing.JTextField txtExpresion;
+    public javax.swing.JTextField txtExpresion2;
     public javax.swing.JTextArea txtResultado;
+    public javax.swing.JLabel txtTitulo;
+    public javax.swing.JDialog wdwMensaje;
     // End of variables declaration//GEN-END:variables
 }
